6eba9c47e95514f2c5969396b3f312108d42115a
FelixKleinpeter <felix.kleinpeter@free.fr>
2020-08-10 13:43:27 +0200

    Voice input

--- utils/input_reader.py
+++ utils/input_reader.py
@@ -1,8 +1,25 @@
 #coding:utf-8
+import speech_recognition as sr
 
 def text_input(question):
     answer = input(question+"\n")
     return answer
 
 def audio_input(question):
-    return question
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print(question)
+        audio = r.record(source, duration=2)
+
+    answer = ""
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        answer =  r.recognize_google(audio)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+    return answer
